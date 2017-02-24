7fd60d3decb547c969fa3d1993deebc710a1fb34
jean-1314 <isfiremsk@gmail.com>
2017-02-24 20:49:32 +0300

    Continue adding style

--- js/script.js
+++ js/script.js
@@ -2,22 +2,32 @@ var enBlock = document.querySelector(".lang-en-link");
 
 var ruBlock = document.querySelector(".lang-ru-link");
 
-enBlock.addEventListener("mouseenter", function(event){
+var welcome = document.querySelector(".welcome-toggle");
+
+var welcomeBtn = document.querySelector(".welcome-btn");
+
+enBlock.addEventListener("mouseenter", function(event) {
   event.preventDefault();
   ruBlock.classList.add("lang-en-link-hover");
 });
 
-enBlock.addEventListener("mouseleave", function(event){
+enBlock.addEventListener("mouseleave", function(event) {
   event.preventDefault();
   ruBlock.classList.remove("lang-en-link-hover");
 });
 
-ruBlock.addEventListener("mouseenter", function(event){
+ruBlock.addEventListener("mouseenter", function(event) {
   event.preventDefault();
   enBlock.classList.add("lang-ru-link-hover");
 });
 
-ruBlock.addEventListener("mouseleave", function(event){
+ruBlock.addEventListener("mouseleave", function(event) {
   event.preventDefault();
   enBlock.classList.remove("lang-ru-link-hover");
+});
+
+welcomeBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  welcome.classList.toggle("welcome-toggle-on");
+  welcomeBtn.classList.toggle("welcome-btn-up");
 });
