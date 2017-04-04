d048f4e0d32df111c96fdb9336819bd566aa345f
jean-1314 <isfiremsk@gmail.com>
2017-04-04 15:54:19 +0300

    Modify file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
 
-gulp.task('default', function() {
-  
+gulp.task("sass", function() {
+  gulp.src("sass/style.scss")
+  .pipe(sass())
+  .pipe(gulp.dest("css"));  
 });
