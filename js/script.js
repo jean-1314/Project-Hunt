0b97e99b55bb746887d15872b56c78b99cea1909
jean-1314 <isfiremsk@gmail.com>
2017-04-11 17:16:52 +0300

    Add resume button

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 var enBlock = document.querySelector(".language-switcher__lang-link--en");
-
 var ruBlock = document.querySelector(".language-switcher__lang-link--ru");
 
 var navMain = document.querySelector('.main-nav');
-
 var navToggle = document.querySelector('.main-nav__toggle');
 
+var resumeToggle = document.querySelector('.resume__button');
+var resumeIconUp = document.querySelector('.resume__button-icon--up');
 
 navMain.classList.remove('main-nav--nojs');
+resumeToggle.classList.remove('resume__button--nojs');
 
 enBlock.addEventListener("mouseenter", function(event) {
   event.preventDefault();
@@ -39,5 +40,24 @@ navToggle.addEventListener('click', function() {
   else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+  }
+});
+
+document.getElementById('button-up').style.display = 'none';
+
+resumeToggle.addEventListener('click', function() {
+
+  if (resumeToggle.classList.contains('resume__button--on')) {
+    resumeToggle.classList.remove('resume__button--on');
+    resumeToggle.classList.add('resume__button--off');
+    document.getElementById('button-up').style.display = 'none';
+    document.getElementById('button-down').style.display = 'block';
+  }
+
+  else {
+    resumeToggle.classList.add('resume__button--on');
+    resumeToggle.classList.remove('resume__button--off');
+    document.getElementById('button-up').style.display = 'block';
+    document.getElementById('button-down').style.display = 'none';
   }
 });
