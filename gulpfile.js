24e2488ba34c8fc8163d4350b051a3a95d9dfb35
jean-1314 <isfiremsk@gmail.com>
2017-04-23 00:54:29 +0300

    Prepare build

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,20 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var minify = require("gulp-minify");
+var del = require("del");
+
 var server = require("browser-sync").create();
 
 var rename = require("gulp-rename");
 var svgstore = require("gulp-svgstore");
 var svgmin = require("gulp-svgmin");
+var imagemin = require("gulp-imagemin");
+
+var run = require("run-sequence");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -16,10 +25,14 @@ gulp.task("style", function() {
   .pipe(postcss([
     autoprefixer({browsers: [
       "last 2 versions"
-    ]})
+    ]}),
+    mqpacker({
+      sort: true
+    })
   ]))
-
   .pipe(gulp.dest("css"))
+  .pipe(minify())
+  .pipe(rename("style.min.css"))
   .pipe(server.stream());
 });
 
@@ -61,3 +74,45 @@ gulp.task("sedona", function(){
     .pipe(rename("sedona.svg"))
     .pipe(gulp.dest("img"));
 });
+
+gulp.task("images", function() {
+  return gulp.src("img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("img"));
+})
+
+gulp.task("compress", function() {
+  gulp.src("js/*.js")
+    .pipe(minify({
+        ext:{
+            min:"min.js"
+        },
+    }))
+    .pipe(gulp.dest("js"))
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "gllacy/**",
+    "sedona/**",
+    "docs/**",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("build", function(fn) {
+  run("clean", "copy", "style", "images", "compress", fn);
+})
+
